e34f636c1e8d72b32c9dbbb376bcd3f35e1d33ee
Nadin Chan nadinchann@gmail.com <nadinchann@gmail.com>
2024-07-20 13:11:47 +0800

    changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_13_024926) do
-
+ActiveRecord::Schema[7.1].define(version: 2024_07_16_093656) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -85,6 +84,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_13_024926) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "host"
+    t.float "latitude"
+    t.float "longitude"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
@@ -92,4 +93,3 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_13_024926) do
   add_foreign_key "bookings", "workspaces"
   add_foreign_key "reviews", "workspaces"
 end
-
